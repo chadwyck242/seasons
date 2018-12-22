a5cb1fb94973113fc920ef8e674cb55bb588690b
chadwyck242 <chadwyck.codeart@gmail.com>
2018-12-22 14:17:32 -0700

    Refactor index.js to add helper function renderContent() to avoid conditional statements in the main render function

--- src/index.js
+++ src/index.js
@@ -12,8 +12,7 @@ class App extends React.Component {
     );
   }
 
-  // React says we have to define render()
-  render() {
+  renderContent() {
     if (this.state.errorMessage && !this.state.lat) {
       return <div>Error: {this.state.errorMessage}</div>
     }
@@ -23,6 +22,14 @@ class App extends React.Component {
     }
 
     return <Spinner message="Please accept location request" />
+  }
+
+  render() {
+    return (
+      <div className="border red">
+        { this.renderContent() }
+      </div>
+    )
   }
 }
 
